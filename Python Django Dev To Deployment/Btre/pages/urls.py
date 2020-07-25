657321ac4f24c92e805018cbe9ffb977d0b63bd8
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-25 20:31:25 +0000

    Implementing url for listings app and on settings, Btre

--- Python Django Dev To Deployment/Btre/pages/urls.py
+++ Python Django Dev To Deployment/Btre/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
- 
+
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('about', views.about, name='about'),
+    path('', views.index, name='index'),
+    path('listing/', views.index, name='listings.urls'),
+    path('about', views.about, name='about'),
 ]
