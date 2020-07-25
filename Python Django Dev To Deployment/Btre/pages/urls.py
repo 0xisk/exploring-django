fd43281a101beb555a2368c7201f15c51968d7c0
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-25 20:44:00 +0000

    Implementing an initial views for listings app, Btre

--- Python Django Dev To Deployment/Btre/pages/urls.py
+++ Python Django Dev To Deployment/Btre/pages/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('listing/', views.index, name='listings.urls'),
     path('about', views.about, name='about'),
 ]
